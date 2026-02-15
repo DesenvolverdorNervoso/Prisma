049fdebd197f5483ce72212e67f8fc7894985149
DesenvolverdorNervoso <diamondwashbage@gmail.com>
2026-02-15 13:53:55 -0300

    Update src/lib/supabaseClient.ts to include Supabase client initialization

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,3 +4,5 @@ const supabaseUrl ="https://baxhbjkvunmjqwablmzb.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJheGhiamt2dW5tanF3YWJsbXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwOTk3MTcsImV4cCI6MjA4NjY3NTcxN30._UfcqiRgX7DhXfFElpZ4AM0AOquUGbW6KQ8CLivu9T4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("Supabase URL carregada:", supabaseUrl);
